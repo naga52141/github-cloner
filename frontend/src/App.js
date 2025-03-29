7a1d23b7a06661bf06c07440236b4a38a04c9a89
naga52141 <122508603+naga52141@users.noreply.github.com>
2025-03-29 11:35:03 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -126,11 +126,7 @@ console.log("Is Private:", isPrivate);
     input.click();
   };
 
-<<<<<<< HEAD
-=======
 
-
->>>>>>> 3ac458f (updated)
   return (
     <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white p-4">
       {/* ✅ GitHub OAuth Login Section */}
